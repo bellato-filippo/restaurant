99e55e90bd90b5ad830304d033d089d4dac66f87
filippo-bellato <filippo.bellato@studenti.unipd.it>
2022-06-05 14:40:14 +0200

    finished. Refresh of the component needs review

--- src/app/plate/plate.component.ts
+++ src/app/plate/plate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Has } from '../models/has.model';
 import { Ingredient } from '../models/ingredient.model';
 import { Plate } from '../models/plate.model';
 import { AuthService } from '../services/auth.service';
@@ -14,41 +15,66 @@ import { HttpService } from '../services/http.service';
 export class PlateComponent {
 
 
-  //plates: Plate[] = [];
   plate: Plate = new Plate();
-  id: number;
   plateEdit: boolean;
   ingredients: Ingredient[] = [];
+  has: Has[] = [];
+  plateIngredient: Ingredient[] = [];
 
-  constructor(public route: ActivatedRoute, public httpService: HttpService, public authService: AuthService) {
+  constructor(public route: ActivatedRoute, public httpService: HttpService, public authService: AuthService, public router: Router) {
     this.plateEdit = false;
     this.httpService.getIngredients().subscribe(res => {
       this.ingredients = res;
     });
-    console.log("loads");
-    /*
-    this.httpService.getPlates().subscribe(res => {
-      this.plates = res;
+    this.httpService.getHas().subscribe(res => {
+      this.has = res;
     });
-    */
     let id = this.route.snapshot.paramMap.get('id') ?? "0";
-    this.id = +id;
-    /*
-    this.httpService.getPlateById().subscribe(res => {
+    this.httpService.getPlateById(id).subscribe(res => {
       this.plate = res;
     });
-    */
+
+    this.httpService.getIngredientsByPlateId(id).subscribe(res => {
+      this.plateIngredient = res;
+    });
+
+    let indexes: number[] = [];
+    this.has.forEach(element => {
+      if (element.plate == this.plate.id) {
+        indexes.push(element.ingredient);
+      }
+    });
    }
 
    edit() {
      this.plateEdit = !this.plateEdit;
     }
 
-    addIngredient(ingredientId: number) {
-      this.ingredients.forEach((element, index) => {
-        if (element.id == ingredientId) {
+    addIngredient(plateId: number, ingredientId: number) {
+      let found = false;
+      this.has.forEach(element => {
+        if (element.ingredient == ingredientId && element.plate == plateId) {
+          alert('Ingredient already exists');
+          found = true;
           return;
         }
+      });
+      if (!found) { 
+        this.httpService.postHas(new Has(plateId, ingredientId));
+        //not very elegant. Does the job for now
+        this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
+          this.router.navigate(['/plate/' + plateId]);
+      }); 
+      } else {
+        return;
+      }
+    }
+
+    removeIngredient(plateId: number, ingredientId: number) {
+      console.log('Called!');
+      this.httpService.deleteHas(plateId, ingredientId);
+      this.router.navigateByUrl('/RefreshComponent', {skipLocationChange: true}).then(() => {
+        this.router.navigate(['/plate/' + plateId]);
       });
     }
 }
