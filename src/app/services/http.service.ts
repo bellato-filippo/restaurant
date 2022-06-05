47cbef92980b8adcea409b794b57f1b216ad2646
filippo-bellato <filippo.bellato@studenti.unipd.it>
2022-06-05 14:39:44 +0200

    all http methods are there

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -31,18 +31,59 @@ export class HttpService {
     }
 
     postPlate(plate: Plate) {
-        this.http.post<Plate>(this.ROOT_URL + '/plates/new', plate);
+        this.http.post<Plate>(this.ROOT_URL + '/plates/new', plate, {
+            responseType: 'text' as 'json'
+        }).subscribe(res => {
+            console.log('Plate created!');
+        });
     }
 
     postIngredient(ingredient: Ingredient) {
-        this.http.post<Ingredient>(this.ROOT_URL + '/ingredients/new', ingredient);
+        this.http.post<Ingredient>(this.ROOT_URL + '/ingredients/new', ingredient, {
+            responseType: 'text' as 'json'
+        }).subscribe(res => {
+            console.log('Ingredient created!');
+        });
     }
+
 
     postHas(has: Has) {
-        this.http.post<Has>(this.ROOT_URL + '/has/new', has);
+        this.http.post<Has>(this.ROOT_URL + '/has/new', has, {
+            responseType: 'text' as 'json'
+        }).subscribe(res => {
+            console.log('Has created!');
+        });
     }
 
-    getPlateById(): Observable<Plate> {
-        return this.http.get<Plate>(this.ROOT_URL + "/plate/6");
+    getPlateById(id: string): Observable<Plate> {
+        return this.http.get<Plate>(this.ROOT_URL + "/plate/" + id);
+    }
+
+    getIngredientsByPlateId(id: string): Observable<Ingredient[]> {
+        return this.http.get<Ingredient[]>(this.ROOT_URL + "/plate/" + id + "/ingredients");
+    }
+
+    deletePlate(id: string) {
+        this.http.delete(this.ROOT_URL + "/plate/" + id,{
+            responseType: 'text'
+        }).subscribe(res => {
+            console.log('Plate deleted!');
+        });
+    }
+
+    deleteIngredient(id: string) {
+        this.http.delete(this.ROOT_URL + "/ingredient/" + id,{
+            responseType: 'text'
+        }).subscribe(res => {
+            console.log('Ingredient deleted!');
+        });
+    }
+
+    deleteHas(plateId: number, ingredientId: number) {
+            this.http.delete(this.ROOT_URL + "/has/" + plateId + "/" + ingredientId, {
+            responseType: 'text'
+        }).subscribe(res => {
+            console.log('Has deleted!');
+        });
     }
 }
